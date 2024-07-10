fca4de21f1f37bb069b58e6101f533029c19667e
Zlindsay26 <zlindsay260602@gmail.com>
2024-07-10 19:45:43 -0400

    add Buttons to javascript
    added buttons into script using getElementById

--- calc.js
+++ calc.js
@@ -2,6 +2,25 @@ let num1 = 0;
 let num2 = 0;
 let operator;
 
+const button7 = document.getElementById('7');
+const button8 = document.getElementById('8');
+const button9 = document.getElementById('9');
+const divideBtn = document.getElementById('/');
+const button4 = document.getElementById('4');
+const button5 = document.getElementById('5');
+const button6 = document.getElementById('6');
+const multiplyBtn = document.getElementById('*');
+const button1 = document.getElementById('1');
+const button2 = document.getElementById('2');
+const button3 = document.getElementById('3');
+const subtractBtn = document.getElementById('-'); 
+const button0 = document.getElementById('0');
+const decimalBtn = document.getElementById('.');
+const equalBtn = document.getElementById('=');
+const addBtn = document.getElementById('+');
+const backspace = document.getElementById('backspace');
+const clearBtn = document.getElementById('clear');
+
 
 function add(a, b){
 return a + b;
@@ -20,5 +39,5 @@ return a / b;
 }
 
 function operate(){
-    
+
 }
